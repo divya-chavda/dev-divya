b235b2f844b3f246d0f3dc00a302009e887cda74
divya-chavda <divyachavda30@gmail.com>
2024-12-20 23:13:10 +0530

    footer and skills design completed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent {
-
+  public menuItems = [
+    {
+      menuTitle: "Resume",
+      menuLink: "",
+    },
+    {
+      menuTitle: "Email",
+      menuLink: "",
+    },
+    {
+      menuTitle: "LinkedIn",
+      menuLink: "",
+    },
+    {
+      menuTitle: "X",
+      menuLink: "",
+    },
+  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
+  combinedSkills = [
+    "java",
+    "spring",
+    "angular",
+    "sql",
+    "kafka",
+    "openshift",
+    "nodejs",
+    "express",
+    "js",
+    "ts",
+    "html",
+    "css",
+    "github",
+    "bitbucket",
+    "materialui",
+    "npm",
+    "mongodb",
+    "leetcode",
+    "jquery",
+  ];
+  lengths = {
+    skills: 9,
+    midSkills: 6,
+    secondTopSkills: 3,
+    topSkill: 1,
+  };
+  skills: String[] = [];
+  midSkills: String[] = [];
+  secondTopSkills: String[] = [];
+  topSkill: String[] = [];
+  ngOnInit() {
+    const shuffledSkills = [...this.combinedSkills].sort(
+      () => Math.random() - 0.5
+    );
 
+    this.skills = shuffledSkills.splice(0, this.lengths.skills);
+    this.midSkills = shuffledSkills.splice(0, this.lengths.midSkills);
+    this.secondTopSkills = shuffledSkills.splice(
+      0,
+      this.lengths.secondTopSkills
+    );
+    this.topSkill = shuffledSkills.splice(0, this.lengths.topSkill);
+  }
 }
